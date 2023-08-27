c7ef6cbe5d14cb1e96d48e348039aea3d9eb4b78
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2023-08-27 10:54:37 +0530

    A and B

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
     public static void main(String[] args) {
         
-        System.out.println("is is dg");
+        System.out.println("print A and B  value");
         int a=50;
         int b=10;
  
