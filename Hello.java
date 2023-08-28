03169ae9f1d7719c7f2b9a1937a42ddd0646c066
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2023-08-28 07:53:58 +0530

    added

--- Hello.java
+++ Hello.java
@@ -9,6 +9,23 @@ public class Hello {
         int b=10;
  
       System.out.println(a+"\n"+b);
+      dislpay();
+
+
+    }
+
+    public static void dislpay()
+    {
+        int num1=10;
+        int num2=10;
+        if(num1==num2)
+        {
+            System.out.println("num1 and num2 are equal"+num1+""+num2);
+        }
+        else
+        {
+          System.out.println("num1 and num2 is not equal");
+        }
 
     }
 
